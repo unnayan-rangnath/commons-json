77390d01d3807711f9a92a733e9dcf6f5fabb97c
Satyam <swakekar@xpanxion.com>
2020-03-19 19:42:47 +0530

    SCOOP-8437 Added GenericEvent to the class

---
+++ src/main/java/com/scoperetail/commons/dto/EraserPayload.java
@@ -0,0 +1,19 @@
+package com.scoperetail.commons.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EraserPayload {
+  private List<Integer> orderIds;
+}
